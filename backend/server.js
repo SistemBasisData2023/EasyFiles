7724216c46239e5041026debedd24eb033cdedab
Zaki Ananda <zaki.ananda@ui.ac.id>
2023-06-07 15:40:03 +0700

    Added auth API

---
+++ backend/server.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const { Client } = require("pg");
+const bp = require('body-parser');
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require('dotenv');
+dotenv.config()
+
+const app = express();
+app.use(bp.json());
+app.use(bp.urlencoded({extended:true}))
+
+
+const db = new Client({
+	connectionString: process.env.SQL_CONNSTRING,
+    sslmode: "require",
+    ssl: true
+});
+
+db.connect((err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('Database succesfuly reached.')
+})
+
+//Auth API
+const login = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		
+		queryResult = await db.query(`SELECT * FROM userTable WHERE userName = '${username}';`);
+		
+		if(queryResult.rows.length == 0){
+			res.send("Invalid username or password");
+			return;
+		}
+		
+		user = queryResult.rows[0];
+		userHash = user.hashedpass;
+		const match = await bcrypt.compare(password, userHash); 
+		
+		
+		if(!match){
+			res.send("Invalid username or password");
+			return;
+		}	
+		
+		userIdentifier = { 
+			username: user.username, 
+			nama: user.namapengguna 
+		};
+		
+		userToken = jwt.sign(userIdentifier, process.env.JWT_SECRET, { expiresIn: "1h" } );
+		
+		res.json({
+			message: "Login succesful",
+			data: userIdentifier,
+			token: userToken
+		})
+		
+	} 
+	catch(error) {
+		res.json({
+			message: "Unknown Error",
+			error: error
+		});
+	}
+}; 
+app.post(`/login`, login);
+
+const register = async (req, res) => {
+	try {
+		const { nama, username, password } = req.body;
+		
+		
+		queryResult = await db.query(`SELECT * FROM userTable WHERE userName = '${username}';`);		
+		if(queryResult.rows.length != 0){
+			res.send("Username taken");
+			return;
+		}
+		
+		const salt = await bcrypt.genSalt(10);
+		const hashedPass = await bcrypt.hash(password, salt);
+		
+		queryResult = await db.query(`INSERT INTO userTable VALUES ('${username}', '${hashedPass}', '${nama}');`);
+
+		res.json({
+			message: "Registration succesful",
+			data: req.body
+		})
+	} 
+	catch(error) {
+		res.json({
+			message: "Unknown Error",
+			error: error
+		});
+	}
+}
+app.post(`/register`, register);
+
+app.post(`/logout`, async (req, res) => {
+	try {
+		//Implement this later
+	} 
+	catch(error) {
+		res.json({
+			message: "Unknown Error",
+			error: error
+		});
+	}
+});
+
+app.listen(9999,()=>{
+    console.log('Listening to Port 9999.')
+})
